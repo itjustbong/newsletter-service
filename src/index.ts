e49ce171909c1f6330a7e7aa691513a0411c83e6
Bong <qhdgkdbs@gmail.com>
2023-01-06 18:11:31 +0900

    Chore: 디버깅용 콘솔 추가

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import 'dotenv/config';
 (async () => {
   const vCrawler = new VelogCrawler();
   const nodemailer = new NodeMailer(process.env.ID || '', process.env.PW || '');
+  console.log(process.env.ID);
 
   const orgHTML = await vCrawler.getHTML();
   const parsedHTML = vCrawler.parseHTML(orgHTML);
