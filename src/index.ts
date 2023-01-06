f4f4612a5b5ea2da598daa9115ab5c48c7d6a657
Bong <qhdgkdbs@gmail.com>
2023-01-06 19:34:04 +0900

    Chore: 벨로그 뉴스레터 서비스 수정

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { VelogCrawler } from './infra/crawler/crawler.velog';
 import { NodeMailer } from './infra/mailer/mailer.nodemailer';
-import { VelogNewsletter } from './service/newsletter/newsletter.velog';
+import { sendNewsLetterForVelog } from './service/newsletter/newsletter.velog';
 import 'dotenv/config';
 
-const velogNewsletter = new VelogNewsletter(
+sendNewsLetterForVelog(
   new NodeMailer(process.env.ID || '', process.env.PW || ''),
   new VelogCrawler()
 );
-
-velogNewsletter.sendMail();
--- src/service/newsletter/newsletter.velog.ts
+++ src/service/newsletter/newsletter.velog.ts
@@ -2,16 +2,16 @@ import { iCrawler } from '../../infra/crawler/crawler.interface';
 import { iMailer } from '../../infra/mailer/mailer.interface';
 import { newsLetterCSS, newsLetterHTML } from '../../template/newsLetter';
 
-export class VelogNewsletter {
-  constructor(private mailer: iMailer, private crawler: iCrawler) {}
-
-  async sendMail() {
-    const orgHTML = await this.crawler.getHTML();
-    const parsedHTML = this.crawler.parseHTML(orgHTML);
-    const newsList = parsedHTML
-      .map(
-        (post) =>
-          `<div class="container" onclick="window.open('${post.link}')">
+export const sendNewsLetterForVelog = async (
+  mailer: iMailer,
+  crawler: iCrawler
+) => {
+  const orgHTML = await crawler.getHTML();
+  const parsedHTML = crawler.parseHTML(orgHTML);
+  const newsList = parsedHTML
+    .map(
+      (post) =>
+        `<div class="container" onclick="window.open('${post.link}')">
             <img src=${post.image}></img>
             <div>
               <div class="title">${post.title}</div>
@@ -19,24 +19,23 @@ export class VelogNewsletter {
               <div class="author">by <b>${post.author}</b></div>
             </div>
         </div>`
-      )
-      .join('');
+    )
+    .join('');
 
-    const templatedView = newsLetterHTML
-      .replace('<news-contents />', newsList)
-      .replace('<style />', newsLetterCSS);
+  const templatedView = newsLetterHTML
+    .replace('<news-contents />', newsList)
+    .replace('<style />', newsLetterCSS);
 
-    const now = new Date();
-    const mailOpt = {
-      from: 'itjustbong@itjustbong.me',
-      to: 'qhdgkdbs@gmail.com',
-      subject: `${now.getMonth() + 1}월 ${now.getDate()}일 Velog 뉴스레터`,
-      html: templatedView,
-    };
-    try {
-      this.mailer.sendMail(mailOpt);
-    } catch (e) {
-      console.log(e);
-    }
+  const now = new Date();
+  const mailOpt = {
+    from: 'itjustbong@itjustbong.me',
+    to: 'qhdgkdbs@gmail.com',
+    subject: `${now.getMonth() + 1}월 ${now.getDate()}일 Velog 뉴스레터`,
+    html: templatedView,
+  };
+  try {
+    mailer.sendMail(mailOpt);
+  } catch (e) {
+    console.log(e);
   }
-}
+};
