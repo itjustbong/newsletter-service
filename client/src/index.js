17c90da91922eeea7debfbd8831cab8d8373c6f1
Bong <qhdgkdbs@gmail.com>
2023-01-07 00:30:49 +0900

    Feat: 클라이언트 폼 연결

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,27 @@
+import { SUPABASE_URL, SUPABASE_KEY } from '../env.js';
 const { createClient } = supabase;
-require('dotenv').config();
 
-supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+supabase = createClient(
+  SUPABASE_URL || process.env.SUPABASE_URL,
+  SUPABASE_KEY || process.env.SUPABASE_KEY
+);
 
-const addUser = async () => {
-  email = document.userInfo.email.value;
-  name = document.userInfo.name.value;
-  service = document.userInfo.service.value;
-  console.log(email);
-  console.log(name);
-  console.log(service);
+const validateEmail = (email) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const addUser = async function () {
+  const data = new FormData(document.userInfo);
+  const name = data.get('name');
+  const email = data.get('email');
+  const service = data.get('service');
 
   if (!name) return alert('이름을 입력해주세요');
   if (!email) return alert('이메일을 입력해주세요');
   if (!service) return alert('서비스를 입력해주세요');
+  if (!validateEmail(email)) return alert('이메일을 유효성을 확인해주세요');
 
   const result = await supabase.from('newsletter').insert([
     {
@@ -25,5 +33,8 @@ const addUser = async () => {
     console.log(error);
     return;
   }
-  console.log(result);
+  alert('뉴스레터 등록에 성공하였습니다.');
+  location.reload();
 };
+
+export { addUser };
