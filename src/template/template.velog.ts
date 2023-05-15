eeb7f3cd2de37713f46e94ccc0164dde0c9ea5ea
Bong <qhdgkdbs@gmail.com>
2023-05-15 19:36:12 +0900

    Feat: 뉴스레터 해지 관련 폼 embeded

--- src/template/template.velog.ts
+++ src/template/template.velog.ts
@@ -3,7 +3,14 @@ import { ParsedSrcType } from '../model/model';
 const newsLetterHTML = `
 <!DOCTYPE html>
 <html lang="ko">
-<body><news-contents /><body>`;
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>News letter service</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body><news-contents />
+  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScHaqeN7Xw9QeW-ch8n7CI5AWAFOeY05tQsc2a9tBkZiad4ag/viewform?embedded=true" width="320" height="451" frameborder="0" marginheight="0" marginwidth="0">로드 중…</iframe>
+<body>`;
 
 export const buildVelogTemplate = (parsedHTML: ParsedSrcType[]) => {
   const newsList = parsedHTML
